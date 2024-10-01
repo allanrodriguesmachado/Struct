367263c77d36895ff1392245a4013b9d09ce58e4
Allan Rodriguea <allanrodriguea@MacBook-Pro-de-Allan.local>
2024-10-01 20:22:13 -0300

    Add tailwind on app.css

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,6 @@ let mix = require('laravel-mix');
 mix.setPublicPath('public')
 mix.version()
 
-mix.css('resources/css/app.css', 'public/css')
+mix.postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss')
+])
